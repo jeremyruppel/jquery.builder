52051a248af39398943849114eaf1b7bc4bc2243
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-03-31 17:44:59 -0700

    add text and options handling

--- src/jquery.builder.js
+++ src/jquery.builder.js
@@ -1,32 +1,21 @@
 ( function( $ )
 {
-  
-  
-  
+  /**
+   * jquery builder
+   *
+   *
+   *
+   */
   var Builder = function( scope, tags )
   {
-    // var self = this;
-    
-    // self.build = function( tag, value, options )
-    // {
-    //   if( typeof value === 'function' )
-    //   {
-    //     
-    //   }
-    //   else
-    //   {
-    //     
-    //   }
-    //   
-    //   return self;
-    // };
-    
     this.build = function( tag, value, options, scope )
     {
+      // Append the tag
       scope.append( '<' + tag + '/>' );
-      
+      // Accept a couple types of values
       switch( typeof value )
       {
+        // If it's a function, alter our scope and call it
         case 'function':
 
           this.scope = $( tag, scope );
@@ -34,23 +23,39 @@
           value.call( this, this );
           
           this.scope = scope;
+
+          break;
+        // If it's a string, add it as text to the node
+        case 'string':
+          
+          $( tag, scope ).text( value );
           
           break;
+        // If it's a hash, apply it as attributes to the node
+        case 'object' :
         
-        case 'string':
-          /*
-            TODO 
-          */
+          $( tag, scope ).attr( value );
+        
           break;
       }
       
+      // If the user wanted to pass text *and* an attribute hash, apply it now
+      if( typeof options === 'object' )
+      {
+        $( tag, scope ).attr( options );
+      }
+      
+      // Return self for chaining
       return this;
     };
     
+    // The scope to start building on
     this.scope = scope;
     
+    // The list of recognized tags
     this.tags = tags;
     
+    // Create a method for each of the tags given
     $( tags ).each( $.proxy( function( index, tag )
     {
       this[ tag ] = function( value, options )
@@ -60,12 +65,19 @@
     }, this ) );
   };
   
+  // A list of default tags to support
+  /*
+    TODO need to make this list a little more legit. 
+  */
   var defaults = 'h1 h2 h3 h4 h5 div input span a'.split( /\s+/ );
   
+  // jquery build plugin
   $.fn.build = function( value )
   {
+    // Accept a couple types of values here, too
     switch( typeof value )
     {
+      // If it's a function, start building!
       case 'function':
         
         var builder = new Builder( this, defaults );
@@ -73,15 +85,15 @@
         value.call( builder, builder );
         
         break;
-      
+      // If it's a string, add it to our custom tags list
       case 'string':
         
         defaults.push( $.trim( value ) );
       
         break;
     }
-
+    // Return jquery for chaining
     return this;
   };
-  
+// Maybe we'll even try this with zepto sometime, who knows?
 } )( jQuery );
--- test/spec/building.js
+++ test/spec/building.js
@@ -46,12 +46,35 @@ describe( 'building nodes', function( )
   {
     it( 'should be able to add text to a node', function( )
     {
+      $( '#test' ).build( function( )
+      {
+        this.h1( 'testing!' );
+      } );
       
+      expect( $( '#test' ).html( ) ).toEqual( '<h1>testing!</h1>' );
     } );
   } );
   
   describe( 'node options', function( )
   {
+    it( 'should take a hash of options and apply them to the node as attributes', function( )
+    {
+      $( '#test' ).build( function( )
+      {
+        this.div( { 'class' : 'active', id : 'testing' } );
+      } );
+
+      expect( $( '#test' ).html( ) ).toEqual( '<div class="active" id="testing"></div>' );
+    } );
     
+    it( 'should be able to accept text as the first parameter and options as the second', function( )
+    {
+      $( '#test' ).build( function( )
+      {
+        this.div( 'testing!', { 'class' : 'active' } );
+      } );
+      
+      expect( $( '#test' ).html( ) ).toEqual( '<div class="active">testing!</div>' );
+    } );
   } );
 } );
